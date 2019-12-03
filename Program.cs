27b8755b7eba914113ffa054abba69bfafe1e83d
tomhosker <tomdothosker@gmail.com>
2019-12-03 09:53:43 +0000

    First commit.

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+using com.caen.RFIDLibrary;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CAENRFIDReader MyReader = new CAENRFIDReader();
+
+            MyReader.Connect(CAENRFIDPort.CAENRFID_TCP, "192.168.0.2");
+
+            CAENRFIDLogicalSource MySource = MyReader.GetSource("Source_0");
+            CAENRFIDLogicalSource MySource2 = MyReader.GetSource("Source_1");
+
+
+            Console.WriteLine("Press ESC to stop");
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    CAENRFIDTag[] MyTags = MySource.InventoryTag();
+                    CAENRFIDTag[] MyTags2 = MySource2.InventoryTag();
+
+                    if (MyTags != null)
+                    {                   
+                        for (int i = 0; i < MyTags.Length; i++)
+                        {
+                
+                            byte[] data = FromHex(BitConverter.ToString(MyTags[i].GetId()));
+                            String s = Encoding.ASCII.GetString(data) + " " + DateTime.Now.ToString("h:mm:ss") + " " + MyTags[i].GetReadPoint() + " " + MyTags[i].GetRSSI().ToString();
+                            Console.WriteLine(s);
+
+                            using (System.IO.StreamWriter file =
+                                new System.IO.StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "output" + DateTime.Now.ToString("d-M-yyyy") + ".txt", true))
+                            {
+                                file.WriteLine(s);
+                            }
+                        }
+                    }
+                    if (MyTags2 != null)
+                    {
+                        for (int i = 0; i < MyTags2.Length; i++)
+                        {
+                            byte[] data = FromHex(BitConverter.ToString(MyTags2[i].GetId()));
+                            String s = Encoding.ASCII.GetString(data) + " " + DateTime.Now.ToString("h:mm:ss") + " " + MyTags2[i].GetReadPoint() + " " + MyTags2[i].GetRSSI();
+                            Console.WriteLine(s);
+
+                            using (System.IO.StreamWriter file =
+                                new System.IO.StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "output" + DateTime.Now.ToString("d-M-yyyy") + ".txt", true))
+                            {
+                                file.WriteLine(s);
+                            }
+                        }
+                    }
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+            MyReader.Disconnect();
+        }
+
+        public static byte[] FromHex(string hex)
+        {
+            hex = hex.Replace("-", "");
+            byte[] raw = new byte[hex.Length / 2];
+            for (int i = 0; i < raw.Length; i++)
+            {
+                raw[i] = Convert.ToByte(hex.Substring(i * 2, 2), 16);
+            }
+            return raw;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# proton-cs-min
+
+This is **minimal program for getting data from the Proton RFID reader using C#**. It was written with the intention of being run on a **Linux** machine, using the **Mono** compiler and runtime environment.
+
+## Getting Started
+
+### Installation and Compilation
+
+1. Download this repository onto your machine.
+1. Open up a terminal.
+1. Navigate, in the terminal, to this repository's directory.
+1. Run "mcs -r:CAENRFIDLibrary.dll Program.cs".
+
+### Run
+
+1. Run "./Program.exe".
